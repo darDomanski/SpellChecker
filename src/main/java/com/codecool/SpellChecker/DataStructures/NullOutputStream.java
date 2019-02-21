1caac3845455ed3b06602064710a2cf423a7b62b
darDomanski <dardomanski@gmail.com>
2019-02-21 15:40:28 +0100

    Create basic project structure

---
+++ src/main/java/com/codecool/SpellChecker/DataStructures/NullOutputStream.java
@@ -0,0 +1,28 @@
+package com.codecool.SpellChecker.DataStructures;
+
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import java.io.OutputStream;
+
+public class NullOutputStream extends OutputStream {
+    public NullOutputStream() {
+    }
+
+    public void close() {
+    }
+
+    public void flush() {
+    }
+
+    public void write(byte[] var1) {
+    }
+
+    public void write(byte[] var1, int var2, int var3) {
+    }
+
+    public void write(int var1) {
+    }
+}
